2f96b70a846d613fa8d65b5469084df6ac773852
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-30 21:17:54 +0800

    validation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+import email_validator
+from wtforms.validators import DataRequired, Email, Length, email_validator
 
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email')
-    password = PasswordField('Password')
+    email = EmailField('Email', validators=[DataRequired(), Email(message="Invalid email")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message="Password must be 8 characters long.")])
+    submit = SubmitField("Login")
+
 app = Flask(__name__)
 app.secret_key = "hawernasdofnwem"
 
@@ -15,9 +19,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
